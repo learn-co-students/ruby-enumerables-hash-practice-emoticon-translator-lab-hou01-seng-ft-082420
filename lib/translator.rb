7983d19f21fc4123bb88d45c722a27f4ed1c1b9f
Stephen Anderson <stephenx11x@icloud.com>
2020-07-30 22:15:26 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def load_library(file_path)
   emoticons_file = YAML.load_file(file_path)
   emoticons_file.each_with_object({}) do |(key,value),new_hash|
-  
+
   english_word = value[0]
   japanese_word = value[1]
    
@@ -11,9 +11,10 @@ def load_library(file_path)
     
    new_hash[key][:english] = english_word
    new_hash[key][:japanese] = japanese_word
-   new_hash
+   new_hash#
   end
 end
+
 
 def get_japanese_emoticon(file_path,w_emoticon)
   emoticons_file = load_library(file_path)
@@ -25,16 +26,27 @@ def get_japanese_emoticon(file_path,w_emoticon)
   end
   japanese_emoticon
  end
-
-def get_english_meaning(file_path,j_emoticon)
-  emoticons_file = load_library(file_path)
-  english_name = "Sorry, that emoticon was not found"
-  emoticons_file.each do |emotion,lang|
-    lang.each do |inner_key,emoticon|
-    if emoticon == j_emoticon
-      then english_name = emotion
-     end
-    end
+ 
+ def get_english_meaning(yaml_file, emoticon)
+  what_we_working_with = load_library("./lib/emoticons.yml")
+  translation = "Sorry, that emoticon was not found"
+  what_we_working_with.each_pair do |mood, mood_hash|
+    if mood_hash[:japanese] == emoticon
+      then translation = mood
   end
-  english_name
 end
+translation
+end
+
+#def get_english_meaning(file_path,j_emoticon)
+  #emoticons_file = load_library(file_path)
+  #english_name = "Sorry, that emoticon was not found"
+  #emoticons_file.each do |emotion,lang|
+    #lang.each do |inner_key,emoticon|
+    #if emoticon == j_emoticon
+      #then english_name = emotion
+    # end
+    #end
+  #end
+  #english_name
+#end
